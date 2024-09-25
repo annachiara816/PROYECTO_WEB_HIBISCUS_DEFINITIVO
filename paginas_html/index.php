c399c7b80872a28975c31a84e0eaf713d0a22234
Gloria <nnaranjoposada@gmail.com>
2024-09-25 23:52:01 +0200

    nuevas imagenes modificadas

--- paginas_html/index.php
+++ paginas_html/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Register</title>
+  <title>Hibiscus</title>
 
   <!-- Google Fonts (Unificada en una sola llamada) -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
