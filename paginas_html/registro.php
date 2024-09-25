c0a7b2ab4832a3dfc8831d5f7b92db9483a9cfdc
Gloria <nnaranjoposada@gmail.com>
2024-09-25 16:29:56 +0200

    cambios

--- paginas_html/registro.php
+++ paginas_html/registro.php
@@ -16,7 +16,7 @@
     rel="stylesheet">
 
   <link rel="icon" type="image/x-icon" href="..\Imagenes\logo1.ico">
-  <link rel="stylesheet" href="..\css\reg_stile.css">
+  <link rel="stylesheet" href="..\css\estilo.css">
   <!-- <link rel="stylesheet" href="..\css\principal.css"> -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
